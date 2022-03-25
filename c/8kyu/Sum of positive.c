32cb157442e1facd92c7d5163132ae07726f8dea
Rel-ativity <davkojak@hotmail.com>
2022-03-25 14:48:12 -0400

    added Sum of positive

---
+++ c/8kyu/Sum of positive.c
@@ -0,0 +1,19 @@
+/*You get an array of numbers, return the sum of all of the positives ones.
+
+Example [1,-4,7,12] => 1 + 7 + 12 = 20
+
+Note: if there is nothing to sum, the sum is default to 0.*/
+
+#include <stddef.h>
+#include <stdio.h>
+
+int positive_sum(const int *values, size_t count){
+  int total = 0;
+  for(int i = 0;i<count;++i){
+    if (values[i] > 0){
+      total += values[i];
+    }
+  }
+  return total;
+}
+ 
